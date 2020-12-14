c7fafe817cad0214832497bcb8d5a9b64ffc21e3
Emerson Gonçalves <emerson.u.g@gmail.com>
2020-12-14 06:00:37 -0300

    Ajuste da função next()

--- node-express/node-express.js
+++ node-express/node-express.js
@@ -3,6 +3,15 @@ const app = express();
 
 const people = require('./people-rotes');
 
+function entrou(req,res,next) {
+        console.log('entrou');
+        next();
+}
+
+app.get( '/entrou', entrou, (req,res)=>{
+    console.log('OK');
+    res.send('OK');
+})
 
 app.get('/nome/:name', (req,res)=>{
     let name = req.params.name;
